06327547dba44bbbe4bca3c153beedcf12aa26fd
Chulwoo Kim <chulwoo.ai@gmail.com>
2018-09-21 23:52:41 -0400

    fix: Modify method 2 of number 14
    There is a problem in the part of initialization of table.
    I fix table initialization using expression values.

--- Chapter8_RecursionAndDynamicProgramming/14.BooleanEvaluation.py
+++ Chapter8_RecursionAndDynamicProgramming/14.BooleanEvaluation.py
@@ -28,6 +28,15 @@ evaluates to true. See what code you can reuse.
 Look at your recursion. Do you have repeated calls anywhere? Can you memoize it?
 """
 
+'''Method 1
+
+This is recursive method. 
+We can see this problem as function(expression, result). 
+We go through all the operators in the expression, 
+so the function(expression, result) splits into two functions; function(expression left, result),
+function(expression right, result) and continues to split it until the length of expression is 1.
+'''
+
 def count_boolean_evaluation(expression, result):
     if len(expression) == 1:
         return 1 if (expression == "1" and result) or (expression == "0" and not result) else 0
@@ -58,22 +67,35 @@ def count_boolean_evaluation(expression, result):
                 count += count_boolean_evaluation(left, False) * count_boolean_evaluation(right, False)
     return count
 
+'''Method 2
 
+The recursion solution has a high time complexity that we have calculated multiple times. 
+We can use 3 dimensions array to store all the values. 
+DP[expression length][expression length][2 – True or False]
+It initializes table using expression.
+
+For example: 1^0|0|1
+initial it would DP[0][0][1] = 1; DP[2][2][0] = 1; DP[4][4][0] = 1; DP[6][6][1] = 1;
+then we start to gap 2 values
+DP[0][2][0 or 1]; DP[2][4][0 or 1]; DP[4][6][0 or 1] from all the previous values we know
+then gap 4 values
+DP[0][4][0 or 1]; DP[2][6][0 or 1];
+then  gap 6 values
+DP[0][6][0 or 1] which is what we want.
+'''
 def count_boolean_evaluation_by_dp(expression, result):
     dp = [[[0, 0] for _ in range(len(expression))] for _ in range(len(expression))]
     for i in range(0, len(dp), 2):
         for j in range(0, len(dp[0]), 2):
             if i == j:
-                dp[i][j] = [1, 1]    
-    print("first : ",dp)
-    for length in range(3, len(expression)+1, 2):
-        print("length : ", length)
-        for start in range(0, len(expression)+1-length, 2):
-            print("start : ",start)
-            for k in range(start+1, start+length, 2):
-                print("k : ",k)
-                end = start + length - 1
-                print("vars : ",length, start, end, k, expression[k])
+                if expression[i] == "1":
+                    dp[i][j][1] = 1
+                else:    
+                    dp[i][j][0] = 1    
+    for length in range(3, len(expression)+1, 2):        
+        for start in range(0, len(expression)+1-length, 2):            
+            for k in range(start+1, start+length, 2):                
+                end = start + length - 1                
                 if expression[k] == "|":
                     dp[start][end][1] += dp[start][k-1][1] * dp[k+1][end][1] + \
                                         dp[start][k-1][0] * dp[k+1][end][1] + \
@@ -88,8 +110,7 @@ def count_boolean_evaluation_by_dp(expression, result):
                     dp[start][end][1] += dp[start][k-1][0] * dp[k+1][end][1] + \
                                         dp[start][k-1][1] * dp[k+1][end][0]
                     dp[start][end][0] += dp[start][k-1][0] * dp[k+1][end][0] + \
-                                        dp[start][k-1][1] * dp[k+1][end][1]
-                print("dp : ", dp[start][end][0], dp[start][end][1], dp)
+                                        dp[start][k-1][1] * dp[k+1][end][1]                
     return dp[0][len(expression)-1][1 if result else 0]
 
 
@@ -102,7 +123,7 @@ class Test(unittest.TestCase):
 
     def test_count_boolean_evaluation_by_dp(self):
         self.assertEqual(count_boolean_evaluation_by_dp("1^0|0|1", False), 2)        
-        #self.assertEqual(count_boolean_evaluation_by_dp("0&0&0&1^1|0", True), 10)
+        self.assertEqual(count_boolean_evaluation_by_dp("0&0&0&1^1|0", True), 10)
 
 if __name__ == "__main__":
     unittest.main()
