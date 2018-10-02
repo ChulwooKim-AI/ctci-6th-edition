266493cee42e078080676a9bcd16d454e779c293
Chulwoo Kim <chulwoo.ai@gmail.com>
2018-10-01 20:33:51 -0400

    feat: create chapter10 and add merge sort

---
+++ Chapter10_SortingAndSearching/MergeSort.py
@@ -0,0 +1,45 @@
+def mergeSort(alist):
+    print("Splitting ",alist)
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",alist)
+
+
+import unittest
+
+class Test(unittest.TestCase):
+    def test_merge_sort(self):
+        alist = [54,26,93,17,77,31,44,55,20]
+        mergeSort(alist)
+        self.assertListEqual(alist, [17, 20, 26, 31, 44, 54, 55, 77, 93])
+        
+
+if __name__ == "__main__":
+    unittest.main()
