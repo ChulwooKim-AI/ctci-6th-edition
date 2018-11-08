7f8a604f29a9e681dba478f3102e43293b16cf71
Chulwoo Kim <chulwoo.ai@gmail.com>
2018-11-07 19:33:58 -0500

    fix: update document and test code

--- Chapter1_ArraysAndStrings/04.PanlindromePermutation.py
+++ Chapter1_ArraysAndStrings/04.PanlindromePermutation.py
@@ -5,8 +5,23 @@ EXAMPLE
 Input: Tact Coa
 Output: True (permutations: "taco cat". "atco cta". etc.)
 
+Hints: 
+#106
+You do not have to-and should not-generate all permutations. This would be very inefficient.
+#121
+What characteristics would a string that is a permutation of a palindrome have?
+#134
+Have you tried a hash table? You should be able to get this down to 0 (N) time.
+#136
+Can you reduce the space usage by using a bit vector?
 """
 
+'''
+Using hashtable, you can solve the problem comparing with count of each characters.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+'''
 from collections import Counter
 
 def checkPelindrome(string):
@@ -18,7 +33,20 @@ def checkPelindrome(string):
         if value % 2:
             checkedOdd += 1
     
-    return True if checkedOdd < 2 else False
+    return checkedOdd < 2
+
+
+import unittest
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.string1 = "Tact Coa"
+        self.string2 = "dogs god"
+    
+    def test_check_panlindrome(self):
+        self.assertTrue(checkPelindrome(self.string1))
+        self.assertTrue(checkPelindrome(self.string2))
+
 
 if __name__ == '__main__':
-    print(checkPelindrome("Tact Coa"))
+    unittest.main()
--- Chapter1_ArraysAndStrings/05.OneWay.py
+++ Chapter1_ArraysAndStrings/05.OneWay.py
@@ -1,12 +1,32 @@
 """There are three types of edits that can be performed on strings: insert a character,
 remove a character, or replace a character. Given two strings, write a function to check if they are
 one edit (or zero edits) away.
+
 EXAMPLE
 pale, ple -> true
 pales. pale -> true
 pale. bale -> true
 pale. bake -> false
+
+Hints: 
+#23
+Start with the easy thing. Can you check each of the conditions separately?
+#97
+What is the relationship between the "insert character" option and the "remove character"
+option? Do these need to be two separate checks?
+#130
+Can you do all three checks in a single pass?
 """
+
+'''
+Using hashtable, we can define edit cases following this:
+First case: characters of longer string are not in shorter string
+Second case: Even though they have same length, character of one string is not in another one.
+Third case: they have a same character but different count.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+'''
 
 from collections import Counter
 
@@ -20,8 +40,7 @@ def checkStatus(shorter, longer):
     return status
 
 def compareStrings(source, target):
-    status = 0
-    
+    status = 0    
     sourceCount = Counter(source)
     targetCount = Counter(target)
 
@@ -29,8 +48,20 @@ def compareStrings(source, target):
         status = checkStatus(targetCount, sourceCount)
     else:
         status = checkStatus(sourceCount, targetCount)
-    return True if status == 1 else False
+    
+    return status == 1
+
+
+import unittest
+
+class Test(unittest.TestCase):
+    def test_compare_string(self):        
+        self.assertTrue(compareStrings("pale", "ple"))
+        self.assertTrue(compareStrings("pales", "pale"))
+        self.assertTrue(compareStrings("pale", "bale"))
+        self.assertFalse(compareStrings("pale", "bake"))
+
 
 if __name__ == "__main__":
-    print(compareStrings("pale", "bake"))
+    unittest.main()
 
